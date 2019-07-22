62a7a95f1e0ea41dd3aa55ecdb2ee9309ee38bcd
Dawid Kurkiewicz <dawid.kurkiewicz@billennium.com>
2019-07-22 09:44:11 +0200

    checking all the between checkboxes on shift click

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
 checkboxes.forEach(checkbox => checkbox.addEventListener("click", handleCheck))
+let lastChecked;
 function handleCheck(e) {
-    console.log(e)
+    let inBetween = false;
     if (e.shiftKey && this.checked) {
-
+        checkboxes.forEach(checkbox=> {
+            if(checkbox === this || checkbox === lastChecked){
+                inBetween = !inBetween;
+            }
+            if(inBetween){
+                checkbox.checked = true;
+            }
+        })
     }
     lastChecked = this;
 }
-let lastChecked;
